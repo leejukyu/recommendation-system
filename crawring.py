8eadc95f78e50c5fa3a80a174c4b2732498f49ec
Name <dlwr777@gmail.com>
2021-07-05 16:16:01 +0900

    genre_link

--- crawring.py
+++ crawring.py
@@ -5,36 +5,33 @@ import re
 def get_movie_link(url):
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html5lib')
-
     movie_links = soup.select('a[href]')
-
     movie_links_list = []
-
     for link in movie_links:
         if re.search(r'st=mcode&sword' and r'&target=after', link['href']):
-            target_url = r'http://movie.naver.com/movie/point/af/list.nhn' + str(link['href'])
+            target_url = r'http://movie.naver.com/movie/point/af/list.nhn'+str(link['href'])
             movie_links_list.append(target_url)
-
+            print(target_url)
     return movie_links_list
 
+url = 'http://movie.naver.com/movie/point/af/list.nhn'
+movie_links = get_movie_link(url)
+# print(movie_links)
 
-def genre_list(url):
+res = requests.get('https://movie.naver.com/movie/point/af/list.nhn?st=mcode&sword=189050&target=after')
+
+def genre_link(url):
     movie_links_list = get_movie_link(url)
     genre_list=[]
-
     for movie_url in movie_links_list:
-        res = requests.get(movie_url)  
+        res = requests.get(movie_url)
         content = res.text
         soup = BeautifulSoup(content, 'html5lib')
-        genre = soup.find_all('table', class_='info_area')
-
-    for genre in genre:
-        genre_list.append(genre.a.get_text())
-
+        genre = soup.find_all('table',class_='info_area')
+        for genre in genre:
+            genre_list.append(genre.a.get_text())
     return genre_list
 
 url = 'http://movie.naver.com/movie/point/af/list.nhn'
-# movie_links = get_movie_link(url)
-# print(movie_links)
-genre_list_data = genre_list(url)
-print(genre_list_data)
+genre_link_data = genre_link(url)
+print(genre_link_data)
