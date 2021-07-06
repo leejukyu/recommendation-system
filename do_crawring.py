6f50cf24703162997227943361131cd935ca1cbd
Name <dlwr777@gmail.com>
2021-07-06 18:06:22 +0900

    Movie_recommender

--- do_crawring.py
+++ do_crawring.py
@@ -4,8 +4,14 @@ from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import ProcessPoolExecutor
 
 page = 13323069
+# page=str(page)
+# url = f"http://movie.naver.com/movie/point/af/list.nhn?st=nickname&sword={page}&target=after"
 
-while page > 13321069 :
+# with ThreadPoolExecutor(max_workers=3) as executor:
+#     executor.submit(crawring.do_crawl, url)
+
+
+while page > 13322569 :
     page=str(page)
     url = f"http://movie.naver.com/movie/point/af/list.nhn?st=nickname&sword={page}&target=after"
 
